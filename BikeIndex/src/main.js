b7c4e4b257b9c1765b5494a7339ea5f37ae44db4
Nick Ramsey and Tae Lee <nick@>
2019-07-03 16:43:33 -0700

    this is last commit

--- BikeIndex/src/main.js
+++ BikeIndex/src/main.js
@@ -14,18 +14,19 @@ $(document).ready(function() {
     request.open("GET", url, true);
     request.send();
     const getElements = function(response) {
-      let date = new Date();
+      let date = new Date(); //date in milliseconds
       let oneWeekBefore = date - 604000000;
-      // console.log(oneWeekBefore);
       let result = [];
       for (let i = 0; i < 99; i++) {
+        // ${response.bikes[i].date_stolen} in API is seconds not milliseconds and so need to 1000 times
         if (parseInt(`${response.bikes[i].date_stolen}`)*1000 > oneWeekBefore) {
-          result.push(`${response.bikes[i].date_stolen}`);
-          console.log(result.length);
+          result.push(`${response.bikes[i].title}`);
         }
+        $('#result').text("The model of " + result + " " + result.length + " bikes are stolen around portland area at last week!");
       }
-      // $('#result').text(`The stolen bikes are ${response.bikes.date_stolen}`);
-      // console.log(result.length);
     };
   });
 });
+
+// console.log(result.length);
+// console.log(oneWeekBefore);
