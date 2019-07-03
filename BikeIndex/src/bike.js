395d5faae02436710c87e12074ed1a4bc0123459
Nick Ramsey and Tae Lee <nick@>
2019-07-03 15:20:01 -0700

    this works without seperation of logics

---
+++ BikeIndex/src/bike.js
@@ -0,0 +1,14 @@
+export function Bike(side1, side2, side3) {
+  this.side1 = side1;
+  this.side2 = side2;
+  this.side3 = side3;
+}
+
+Triangle.prototype.checkType = function() {
+
+};
+
+let date = new Date();
+const oneWeek = function (date) {
+  return date - 604000000
+}
--- BikeIndex/src/main.js
+++ BikeIndex/src/main.js
@@ -1,29 +1,31 @@
 import './styles.css';
-// import { Triangle } from './triangle.js';
+// import { Bike } from './bike.js';
 
 $(document).ready(function() {
   $('#bike').click(function() {
-    console.log("It works");
-    // const city = $('#location').val();
-    // $('#location').val("");
-
     let request = new XMLHttpRequest();
-    // const url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=IP&distance=10&stolenness=stolen&access_token=api_key`;
-    const url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=1&location=IP&distance=10&stolenness=stolen&access_token=api_key`;
-
+    const url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=100&location=%E2%80%9CPortland%2C%20OR%E2%80%9D&distance=10&stolenness=proximity&access_token=api_key`;
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
         getElements(response);
       }
     };
-
     request.open("GET", url, true);
     request.send();
-
     const getElements = function(response) {
-      $('#result').text(`The stolen bikes are ${response.bikes[0].title}`);
-      console.log(response.bikes[0].title);
+      let date = new Date();
+      let oneWeekBefore = date - 604000000;
+      // console.log(oneWeekBefore);
+      let result = [];
+      for (let i = 0; i < 99; i++) {
+        if (parseInt(`${response.bikes[i].date_stolen}`)*1000 > oneWeekBefore) {
+          result.push(`${response.bikes[i].date_stolen}`);
+          console.log(result.length);
+        }
+      }
+      // $('#result').text(`The stolen bikes are ${response.bikes.date_stolen}`);
+      // console.log(result.length);
     };
   });
 });
