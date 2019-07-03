763b39ce340c44ea8d82f69194285d855db1fb6e
Nick Ramsey and Tae Lee <nick@>
2019-07-03 13:20:19 -0700

    this works for one page API information

--- BikeIndex/src/main.js
+++ BikeIndex/src/main.js
@@ -8,7 +8,8 @@ $(document).ready(function() {
     // $('#location').val("");
 
     let request = new XMLHttpRequest();
-    const url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=IP&distance=10&stolenness=stolen&access_token=api_key`;
+    // const url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=IP&distance=10&stolenness=stolen&access_token=api_key`;
+    const url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=1&location=IP&distance=10&stolenness=stolen&access_token=api_key`;
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
@@ -21,8 +22,8 @@ $(document).ready(function() {
     request.send();
 
     const getElements = function(response) {
-      $('#result').text(`The stolen bikes are ${response}`);
-      console.log(response);
+      $('#result').text(`The stolen bikes are ${response.bikes[0].title}`);
+      console.log(response.bikes[0].title);
     };
   });
 });
